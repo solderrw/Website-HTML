81b061051e207f00ef6ab206ec346ce4ed3ec9f9
solderrw <101395749+solderrw@users.noreply.github.com>
2022-04-21 01:42:39 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.slider').slick({
+    
+        Infinite:true,
+        autoplay:true,
+        autoplaySpeed: 5000,
+        adaptiveHeight: true,
+        
+        
+        });
+
+});
